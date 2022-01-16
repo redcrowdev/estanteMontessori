517ca186065bbfdacc84477600e9806645765976
Erik Wroblewski <red.crow.dev@outlook.com>
2022-01-16 15:50:02 -0300

    Adicionado ejs-mate para criação de templates. Adicionado CSS do bootstrap 5, mas não está funcionando. Talvez a configuração do ambiente, que usa WSL?

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const Activity = require('./models/activity.js');
 
@@ -19,6 +20,7 @@ db.once('open', () => {
 
 //App configuration
 const app = express();
+app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
