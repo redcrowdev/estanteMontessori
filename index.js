b7a91bbd59723906957f906eae41606abd61dc65
Erik Wroblewski <red.crow.dev@outlook.com>
2022-01-19 21:24:32 -0300

    Adicionados tratamentos de erro BEM BÁSICOS para finalidade de teste.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const Activity = require('./models/activity.js');
+const appError = require('./appError');
 
 //DB connection
 mongoose.connect('mongodb://192.168.15.2:27017/estanteMontessori', {
@@ -12,7 +13,7 @@ mongoose.connect('mongodb://192.168.15.2:27017/estanteMontessori', {
 });
 
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
+db.on('error', console.error.bind(console, 'Connection Error'));
 db.once('open', () => {
    console.log('Database Connected');
 });
@@ -21,6 +22,7 @@ db.once('open', () => {
 const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
 // app.use(express.static('public'))
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
@@ -38,10 +40,6 @@ app.get('/atividades', async (req, res) => {
    res.render('atividades/index', { activities })
 })
 
-app.get('/teste', async (req, res) => {
-   res.render('test');
-})
-
 app.get('/atividades/nova-atividade', (req, res) => {
    res.render('atividades/nova-atividade')
 })
@@ -52,8 +50,12 @@ app.post('/atividades', async (req, res) => {
    res.redirect(`/atividades/${activity._id}`)
 })
 
-app.get('/atividades/:id', async (req, res) => {
+app.get('/atividades/:id', async (req, res, next) => {
    const activity = await Activity.findById(req.params.id)
+   if (!activity) {
+      res.status(404).render('404');
+      //next(new appError('Atividade não Encontrada', 404));
+   }
    res.render('atividades/detalhes', { activity });
 });
 
@@ -65,6 +67,9 @@ app.get('/atividades/:id/editar', async (req, res) => {
 app.put('/atividades/:id', async (req, res) => {
    const { id } = req.params;
    const activity = await Activity.findByIdAndUpdate(id, { ...req.body.activity });
+   if (!activity) {
+      res.status(404).render('404');
+   }
    res.redirect(`/atividades/${activity._id}`)
 })
 
@@ -76,7 +81,17 @@ app.delete('/atividades/:id', async (req, res) => {
 
 // Page not found handler
 app.use((req, res) => {
-   res.status(404).send('Não Encontrado!')
+   res.status(404).render('404')
+})
+
+//Very basic error handling
+app.use((err, req, res, next) => {
+   const { status = 500 } = err;
+   if (status = 404) {
+      res.status(status).render('404')
+   } else {
+      res.status(status).render('500')
+   }
 })
 
 //App service
