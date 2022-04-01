ce352e95f9e60833d83e52bc8ac3164319d1fa63
Erik Wroblewski <red.crow.dev@outlook.com>
2022-04-01 17:29:39 -0300

    variável de BD para Deploy no Heroku

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ const usersRoutes = require('./routes/users.js');
 
 //DB connection
 
-//const dbURL = process.env.dbConnection
-const dbURL = 'mongodb://192.168.15.2:27017/estanteMontessori'
+const dbURL = process.env.dbConnection
+//const dbURL = 'mongodb://192.168.15.2:27017/estanteMontessori'
 //const dbURL = 'mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000'
 
 mongoose.connect(dbURL, {
